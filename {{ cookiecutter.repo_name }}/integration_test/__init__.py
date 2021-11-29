549940e9bdf8419ab6e6b85c60406ec82f2150fa
tbourton <tb@nect.com>
2021-11-29 10:14:55 +0100

    add __init__.py's to tests

---
+++ {{ cookiecutter.repo_name }}/integration_test/__init__.py
@@ -0,0 +1,6 @@
+"""End-to-end tests for {{ cookiecutter.namespace_name }}.
+
+Coverage requires __init__.py to be defined:
+https://coverage.readthedocs.io/en/coverage-5.5/source.html#execution
+
+"""
---
+++ {{ cookiecutter.repo_name }}/service/tests/__init__.py
@@ -0,0 +1,6 @@
+"""Tests for {{ cookiecutter.namespace_name }}.service package.
+
+Coverage requires __init__.py to be defined:
+https://coverage.readthedocs.io/en/coverage-5.5/source.html#execution
+
+"""
